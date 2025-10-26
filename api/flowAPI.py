9c45a8a42d6a03034b0bb4dd7674ae02d22b3669
Naidu <naidu9678@gmail.com>
2025-10-26 16:08:47 +0530

    Obscure Prefect Cloud credentials in flowAPI.py
    Replaced sensitive Prefect Cloud credentials with placeholders.

--- api/flowAPI.py
+++ api/flowAPI.py
@@ -1,10 +1,10 @@
 import requests
 
 # Replace these variables with your actual Prefect Cloud credentials
-PREFECT_API_KEY = "pnu_WN525BrLmbiIO3srOrti5qLCeFrsx113bY0z"  # Your Prefect Cloud API key
-ACCOUNT_ID = "e5a12ba2-a3dc-4d0f-a42c-4b0459f708e9"  # Your Prefect Cloud Account ID
-WORKSPACE_ID = "b073822f-9781-46af-912e-635ec2b0c441"  # Your Prefect Cloud Workspace ID
-FLOW_ID = "644324e6-2355-4109-9428-4e2f94f00254"  # Your Flow ID
+PREFECT_API_KEY = "xxxx"  # Your Prefect Cloud API key
+ACCOUNT_ID = "xxxx"  # Your Prefect Cloud Account ID
+WORKSPACE_ID = "xxxx"  # Your Prefect Cloud Workspace ID
+FLOW_ID = "xxxx"  # Your Flow ID
 
 # Correct API URL to get flow details
 PREFECT_API_URL = f"https://api.prefect.cloud/api/accounts/{ACCOUNT_ID}/workspaces/{WORKSPACE_ID}/flows/{FLOW_ID}"
