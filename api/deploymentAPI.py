42cdb226384e4aa92ceeeead17922042e0304d1c
Naidu <naidu9678@gmail.com>
2025-10-26 16:08:20 +0530

    Replace Prefect Cloud credentials with placeholders
    Updated Prefect Cloud credentials to placeholders for security.

--- api/deploymentAPI.py
+++ api/deploymentAPI.py
@@ -2,10 +2,10 @@
 import requests
 
 # Replace these variables with your actual Prefect Cloud credentials
-PREFECT_API_KEY = "pnu_WN525BrLmbiIO3srOrti5qLCeFrsx113bY0z"  # Your Prefect Cloud API key
-ACCOUNT_ID = "e5a12ba2-a3dc-4d0f-a42c-4b0459f708e9"  # Your Prefect Cloud Account ID
-WORKSPACE_ID = "b073822f-9781-46af-912e-635ec2b0c441"  # Your Prefect Cloud Workspace ID
-DEPLOYMENT_ID = "56ed0044-551a-4854-a2a2-9bb3cc33f22e"  # Your Deployment ID
+PREFECT_API_KEY = "xxxx"  # Your Prefect Cloud API key
+ACCOUNT_ID = "xxxxx"  # Your Prefect Cloud Account ID
+WORKSPACE_ID = "xxxx"  # Your Prefect Cloud Workspace ID
+DEPLOYMENT_ID = "xxxx"  # Your Deployment ID
 
 # Correct API URL to get deployment details
 PREFECT_API_URL = f"https://api.prefect.cloud/api/accounts/{ACCOUNT_ID}/workspaces/{WORKSPACE_ID}/deployments/{DEPLOYMENT_ID}"
